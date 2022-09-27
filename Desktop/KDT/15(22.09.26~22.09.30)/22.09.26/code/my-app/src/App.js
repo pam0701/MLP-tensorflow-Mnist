e9dce98577fbced1f7922124bbcd258b656a985c
pam0701 <psb04027@naver.com>
2022-09-27 17:48:15 +0900

    feat: components 정의

---
+++ Desktop/KDT/15(22.09.26~22.09.30)/22.09.26/code/my-app/src/App.js
@@ -0,0 +1,19 @@
+import TopHeader from "./components/TopHeader";
+import ImgComponent from "./components/ImgComponent";
+import BtnToNaver from "./components/BtnToNaver";
+import ClassState from "./components/ClassState";
+import FuntionalState from "./components/FunctionalState";
+
+function App() {
+  return (
+    <div className="App">
+      <TopHeader />
+      <ImgComponent />
+      <BtnToNaver />
+      <ClassState />
+      <FuntionalState />
+    </div>
+  );
+}
+
+export default App;
